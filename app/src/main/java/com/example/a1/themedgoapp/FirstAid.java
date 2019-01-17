e1b338e0de91087ed3ed2961a18b5a013bb99a7c
Srivarsha Gobburi <srivarshagobburi1@gmail.com>
2019-01-17 20:06:40 +0530

    Initial commit

---
+++ app/src/main/java/com/example/a1/themedgoapp/FirstAid.java
@@ -0,0 +1,32 @@
+package com.example.a1.themedgoapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebView;
+
+public class FirstAid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_aid);
+        WebView webview = (WebView)findViewById(R.id.webview);
+        webview.loadUrl();
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
